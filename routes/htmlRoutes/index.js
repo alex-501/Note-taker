bdbc8404763d03e01ed0f4c7a9cba00899ebd967
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-05-14 14:27:12 -0700

    Update index.js

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,21 +1,11 @@
-//routes
-
+//declare consts path & require
 const path = require('path');
 const router = require('express').Router();
-//GET method requests
 
-//Send method- for short strings
-//requests data from index.html
-router.get('/', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-//request data from notes.html
-router.get('/notes', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/notes.html'));
-});
-//requests data from index.html
-router.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-//helps require statement acquire exported data and objects
+//get routes
+router .get ( '/notes',  (req,res)=> 
+{ res.sendFile(path.join(__dirname, '../../public/notes.html'));});
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));});
+//router export
 module.exports = router;
